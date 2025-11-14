97512692adb22cb8add5c01fcc9ae76b0854eae5
Akshath Kamath <akshathkamathwork@gmail.com>
2025-11-14 17:20:27 -0500

    Added Dockerfile

--- app/main.py
+++ app/main.py
@@ -53,5 +53,5 @@ app.include_router(rooms.router)
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("app.main:app", host="0.0.0.0", port=4000, reload=True)
+    uvicorn.run("app.main:app", host="0.0.0.0", port=8080, reload=True)
 
