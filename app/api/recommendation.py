837b463e2221bbe2191cfb354ed4290ed1b4e543
Akshath Kamath <akshathkamathwork@gmail.com>
2025-12-09 14:29:10 -0500

    Added endpoint for agent

--- app/api/recommendation.py
+++ app/api/recommendation.py
@@ -24,3 +24,19 @@ async def get_matched_rooms(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
             detail="Failed to get matches"
         )
+
+@router.post("/agent")
+async def get_matched_rooms_agent(
+    user: UserFilter,
+    user_id: str,
+    rec_service_obj: RecommendationService = Depends(get_recommendation_service)
+):
+    try:
+        return await rec_service_obj.find_best_match(user_id=user_id, user=user)
+    except Exception as e:
+        logger.error(f"Error in create_room endpoint: {str(e)}")
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Failed to get matches"
+        )
+
